493a9c30f794cb5cf3b7d5b2678896e9a4f33e8b
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-05-21 23:25:49 +0900

    feat: Add optimize method

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+#[wasm_bindgen(catch)]
+pub fn optimize(bytes: &[u8]) -> Result<Vec<u8>, JsValue> {
+    match oxipng::optimize_from_memory(bytes, &oxipng::Options::default()) {
+        Ok(ret) => Ok(ret),
+        Err(_) => Err(JsValue::from_str("Failed optimize...")),
+    }
 }
