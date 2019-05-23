f3919df50627e62a756213df8266edfe406d0af8
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-05-23 22:35:50 +0900

    fix: Fix example

--- examples/node/index.js
+++ examples/node/index.js
@@ -2,10 +2,12 @@ const fs = require("fs");
 const PNG = require("pngjs").PNG;
 const optimize = require("../../pkg/image_compressor").optimize;
 
-const img1 = fs
-  .createReadStream("img1.png")
+fs.createReadStream("img1.png")
   .pipe(new PNG())
-  .on("parsed", function compress(v) {
-    console.log(v);
-    optimize(img1.data);
+  .on("parsed", function compress(data) {
+    try {
+      optimize(Uint8Array.from(data));
+    } catch (error) {
+      console.log("failed optimize PNG: ", error);
+    }
   });
