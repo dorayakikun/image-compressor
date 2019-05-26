c4190d98d6c9ecd54a35fd40cc07049baaccd705
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-05-27 00:34:30 +0900

    fix: Change cargo dependencies

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,13 +1,10 @@
 const fs = require("fs");
-const PNG = require("pngjs").PNG;
 const optimize = require("../../pkg/image_compressor").optimize;
 
-fs.createReadStream("img1.png")
-  .pipe(new PNG())
-  .on("parsed", function compress(data) {
-    try {
-      optimize(Uint8Array.from(data));
-    } catch (error) {
-      console.log("failed optimize PNG: ", error);
-    }
-  });
+fs.createReadStream("img1.png").on("parsed", function handleParsed(data) {
+  try {
+    optimize(Uint8Array.from(data));
+  } catch (error) {
+    console.log("failed optimize: ", error);
+  }
+});
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
+use oxipng;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(catch)]
 pub fn optimize(bytes: &[u8]) -> Result<Vec<u8>, JsValue> {
-    match oxipng::optimize_from_memory(bytes, &oxipng::Options::default()) {
-        Ok(ret) => Ok(ret),
-        Err(_) => Err(JsValue::from_str("Failed optimize...")),
-    }
+  match oxipng::optimize_from_memory(bytes, &oxipng::Options::default()) {
+    Ok(ret) => Ok(ret),
+    Err(ref e) => Err(JsValue::from_str(&format!("{:?}", e))),
+  }
 }
